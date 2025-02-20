ea6f3c916650b5b70a15c2baf83c26a41b946294
josejuniorpg <yunipgx@protonmail.com>
2025-02-20 12:37:00 -0600

    Change /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { ColorSchemeToggle } from '@/src/components/common/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '@/src/components/common/Welcome/Welcome';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
-      <ColorSchemeToggle />
     </>
   );
 }
--- src/components/common/Welcome/Welcome.tsx
+++ src/components/common/Welcome/Welcome.tsx
@@ -1,4 +1,5 @@
-import { Anchor, Text, Title } from '@mantine/core';
+import Link from 'next/link';
+import { Center, List, Stack, Text, Title } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
@@ -7,17 +8,30 @@ export function Welcome() {
       <Title className={classes.title} ta="center" mt={100}>
         Welcome to{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+          Parrot
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit page.tsx file.
+        Es es un proyecto de ejemplo con Mantine y Next.js. El Frotn es en Next 15 y el Back usa
+        Django 5.1
       </Text>
+      <Stack align={'center'}>
+        <List c={'deepGray.9'}>
+          <li>Para acceder a links sera neceesario que crees un Usuarios en el Backend.</li>
+          <li> Ademas tambien, deberas de crearle uno o mas restaurantes.</li>
+          <li>Y por ultimo crear empleados para esos restaurantes.</li>
+        </List>
+        <Title fz={40} c={'deepRed.9'} size="lg">
+          <br />
+          Estas son las ULRS del Fronted.
+          <br />
+        </Title>
+        <Stack>
+          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/select-restaurant">Seleccionar Restaurante</Link>
+          <Link href="/daily-report/products">Reporte Diario</Link>
+        </Stack>
+      </Stack>
     </>
   );
 }
